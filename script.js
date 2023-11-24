f1a20eb57ec9b8c7e96125f06d787863cf84579c
Rohan Gupta <simplyrohan10@gmail.com>
2023-11-24 15:57:30 -0800

    Button Effect

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
+text_start = "I'm Rohan Gupta";
+
 header = document.getElementById("header");
 
-targettext = "simplyrohan"
+for (let element of document.getElementsByClassName("fill")) {
+    let text = element.innerText;
+    element.innerHTML = "";
+    let text_element = document.createElement("span");
+    text_element.innerHTML = text;
+    element.appendChild(text_element);
+
+    let ink = document.createElement("span");
+    ink.className = "ink";
+    element.appendChild(ink);
+
+    element.addEventListener("mouseenter", (e) => {
+        let x = e.clientX - element.getBoundingClientRect().left;
+        let y = e.clientY - element.getBoundingClientRect().top;
+        ink.style.left = x + "px";
+        ink.style.top = y + "px";
+        ink.style.animation = "fill 0.7s ease-in-out forwards";
+    });
+    element.addEventListener("mouseleave", (e) => {
+        let x = e.clientX - element.getBoundingClientRect().left;
+        let y = e.clientY - element.getBoundingClientRect().top;
+        ink.style.left = x + "px";
+        ink.style.top = y + "px";
+        ink.style.animation = "unfill 1.2s ease-in-out forwards";
+    });
+}
+
 i = 0;
 function type() {
-    if (i < targettext.length) {
-        header.innerHTML += targettext.charAt(i);
+    if (i < text_start.length) {
+        header.innerHTML += text_start.charAt(i);
         i++;
         setTimeout(type, 150);
     }
 }
+
+
 type()

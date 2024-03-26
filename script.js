fce072eb4fe06477b550e20064e34398f07d9363
Rohan Gupta <simplyrohan10@gmail.com>
2024-03-26 15:43:14 -0700

    Button Effects

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
-text_start = "I'm Rohan Gupta";
+ink = document.querySelector('.ink')
+button = document.querySelector('.button')
 
-header = document.getElementById("header");
-
-for (let element of document.getElementsByClassName("fill")) {
-    let text = element.innerText;
-    element.innerHTML = "";
-    let text_element = document.createElement("span");
-    text_element.innerHTML = text;
-    element.appendChild(text_element);
-
-    let ink = document.createElement("span");
-    ink.className = "ink";
-    element.appendChild(ink);
-
-    element.addEventListener("mouseenter", (e) => {
-        let x = e.clientX - element.getBoundingClientRect().left;
-        let y = e.clientY - element.getBoundingClientRect().top;
-        ink.style.left = x + "px";
-        ink.style.top = y + "px";
-        ink.style.animation = "fill 0.7s ease-in-out forwards";
-    });
-    element.addEventListener("mouseleave", (e) => {
-        let x = e.clientX - element.getBoundingClientRect().left;
-        let y = e.clientY - element.getBoundingClientRect().top;
-        ink.style.left = x + "px";
-        ink.style.top = y + "px";
-        ink.style.animation = "unfill 1.2s ease-in-out forwards";
-    });
-}
-
-i = 0;
-function type() {
-    if (i < text_start.length) {
-        header.innerHTML += text_start.charAt(i);
-        i++;
-        setTimeout(type, 150);
-    }
-}
-
-
-type()
+button.addEventListener('mousemove', (e) => {
+    ink.style.top = e.clientY - button.getBoundingClientRect().y + "px"
+    ink.style.left = e.clientX - button.getBoundingClientRect().x + "px"
+})
